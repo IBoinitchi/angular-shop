c99dab4fef62c0ca955b3ee9840ccb82a8cb3039
IBoinitchi <ion.boinitchi@amdaris.com>
2024-10-10 17:55:09 +0300

    Initial commit

---
+++ src/app/admin/login-page/login-page.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/auth.service';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit {
+  form: FormGroup;
+  submitted = false;
+
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) { 
+  }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.submitted = true;
+
+    const user = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+      returnSecureToken: true
+    };
+
+    console.log('user',user);
+    console.log('this.authService',this.authService);
+
+    this.authService.login(user).subscribe(res => {
+      console.log('res', res);
+      this.form.reset();
+      this.router.navigate(['/admin', 'dashboard']);
+      this.submitted = false;
+    }, () => {
+      this.submitted = false;
+    });
+  }
+
+}
