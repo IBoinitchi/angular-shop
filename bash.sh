9d766ac5ff233cf3f8712faeef747e90a21ba73c
IBoinitchi <ion.boinitchi@amdaris.com>
2024-10-16 16:27:37 +0300

    test action

--- bash.sh
+++ bash.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Print current directory
-echo "Current directory: $(pwd)"
-
-# List files in the current directory
-echo "Files in current directory:"
-ls
-
 # Display the current date and time
 echo "Current date and time: $(date)"
 
@@ -14,9 +7,11 @@ echo "Current date and time: $(date)"
 cp ./src/environments/environment.ts ./src/environments/environment.prod.ts
 
 # Replace vars
-sed -i 's/apiKey:""/apiKey:"${{ ENV_API_KEY }}"/g' ./src/environments/environment.prod.ts
-sed -i 's/fbDbUrl:""/fbDbUrl:"${{ ENV_FB_DB_URL }}"/g' ./src/environments/environment.prod.ts
+sed -i 's/apiKey:""/apiKey:"$(ENV_API_KEY)"/g' ./src/environments/environment.prod.ts
+sed -i 's/fbDbUrl:""/fbDbUrl:"$(ENV_FB_DB_URL)"/g' ./src/environments/environment.prod.ts
 
 # List env files
 echo "Files in environments directory:"
 ls -la ./src/environments/
+
+cat ./src/environments/environment.prod.ts
