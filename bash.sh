7a402a7e760a1dbb9b805dfcc3dcd4f744e6f1a4
IBoinitchi <ion.boinitchi@amdaris.com>
2024-10-21 14:45:40 +0300

    Optimisation

--- bash.sh
+++ bash.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-echo $ENV_API_KEY
-echo $ENV_FB_DB_URL
-
 # Display the current date and time
 echo "Current date and time: $(date)"
 
 # Copy env
 cp ./src/environments/environment.ts ./src/environments/environment.prod.ts
 
-# Replace vars
-sed -i 's/production: false/production: true/g' ./src/environments/environment.prod.ts
-sed -i "s@apiKey: \"\"@apiKey: \"$ENV_API_KEY\"@" ./src/environments/environment.prod.ts
-sed -i "s@firebaseDBUrl: \"\"@firebaseDBUrl: \"$ENV_FB_DB_URL\"@" ./src/environments/environment.prod.ts
-
 # List env files
 echo "Files in environments directory:"
 ls -la ./src/environments/
 
-cat ./src/environments/environment.prod.ts
+# Replace vars
+sed -i 's/production: false/production: true/g' ./src/environments/environment.prod.ts
+sed -i "s@apiKey: \"\"@apiKey: \"$ENV_API_KEY\"@" ./src/environments/environment.prod.ts
+sed -i "s@firebaseDBUrl: \"\"@firebaseDBUrl: \"$ENV_FB_DB_URL\"@" ./src/environments/environment.prod.ts
