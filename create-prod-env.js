0758c0dd4789713aaf2f4296bc0c0b00ddabff84
IBoinitchi <ion.boinitchi@amdaris.com>
2024-10-23 14:54:07 +0300

    Rewrite process of creation production env

---
+++ create-prod-env.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const dir = "src/environments";
+const file = "environment.ts";
+const prodFile = "environment.prod.ts";
+
+const prodEnvConfig = `${process.env.PROD_ENV_CONFIG}`;
+
+fs.access(dir, fs.constants.F_OK, (err) => {
+	if (err) {
+		console.log("Directory does not exist. Creating now.", process.cwd());
+		fs.mkdir(dir, { recursive: true }, (err) => {
+			if (err) {
+				throw err;
+			}
+		});
+	}
+	try {
+		fs.writeFileSync(dir + "/" + prodFile, prodEnvConfig);
+		if (fs.existsSync(dir + "/" + file) && fs.existsSync(dir + "/" + prodFile)) {
+			console.log("Environment files are created successfully.");
+		}
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+});
